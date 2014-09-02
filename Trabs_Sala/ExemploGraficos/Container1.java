c85244af90623bafccc5ccda54ab3e1faba13704
aluno <aluno@s04m01>
2014-09-02 11:15:44 -0300

    Exercicio graficos 2

---
+++ Trabs_Sala/ExemploGraficos/Container1.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import javax.swing.*;
+
+class Container1 extends Container{
+  
+  public void paint(Graphics g){
+    super.paint(g);
+    g.drawRect(10,10,480,280);
+  }
+
+}
