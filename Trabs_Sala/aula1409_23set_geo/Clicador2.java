28aec131fd57cfa8be560186fdfe5148fdf10bf8
Johnaltan Neves <johnaltan@gmail.com>
2014-09-30 11:42:39 -0300

    Exemplo com poligono

--- Trabs_Sala/aula1409_23set_geo/Clicador2.java
+++ Trabs_Sala/aula1409_23set_geo/Clicador2.java
@@ -1,11 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.awt.event.MouseEvent;
+
 class Clicador {
   //badcode: dependencia ciclica (cf. aula)
   Clicador(Reprodutor quadro) {
     this.quadro = quadro;
+    pontos = new ArrayList<Ponto>();
   }
   private Reprodutor quadro;
   private int q = 0;
-  private int x1, x2, y1, y2;
+  private int x1,y1,x2,y2;
+  private List<Ponto> pontos;
   //via algoritmo
   //mais tarde, via padrao "estado"
   void clique(int x, int y) { //System.out.println("("+ x +","+ y +")");
@@ -25,6 +31,20 @@ class Clicador {
     q = (q+1)%3;
     quadro.repaint();
   }
-
+  
+  void clique(MouseEvent e){
+    if(e.getButton() == MouseEvent.BUTTON3) q = 1;
+    if(q == 0){
+      quadro.addFig(new FigCirculo(e.getX(), e.getY(),5));
+      pontos.add(new Ponto(e.getX(),e.getY()));
+    }
+    if(q == 1){
+      pontos.add(new Ponto(e.getX(),e.getY()));
+      quadro.addFig(new FigPoligono(pontos.toArray(new Ponto[0])));
+      pontos.clear();
+      q = 0;
+    }
+    quadro.repaint();
+  }
 
 }
