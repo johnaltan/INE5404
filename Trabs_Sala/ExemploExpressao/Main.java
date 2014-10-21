dcaa24aca9ab5de67b507b9e18dae8a74b235d50
Johnaltan Neves <johnaltan@gmail.com>
2014-10-21 11:28:33 -0200

    Mais expressoes adicionadas, arquivos de backup retirados

--- Trabs_Sala/ExemploExpressao/Main.java
+++ Trabs_Sala/ExemploExpressao/Main.java
@@ -4,9 +4,11 @@ class Main{
     n2 = new Num(2);
     n3 = new Num(3);
     n4 = new Num(4);
-    Expressao s23, s234;
+    Expressao s23, s234, s2345, s23456;
     s23 = new Soma(n2,n3);
     s234 = new Mod(n4,s23);
-    System.out.println(s234.avaliar());
+    s2345 = new Potencia(new Num(2),s234);
+    s23456 = new RaizQuadrada(s2345);
+    System.out.println(s23456.avaliar());
   }
 }
