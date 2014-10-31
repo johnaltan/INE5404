0d545830967982b1c0b5238a92b0e501ea8353f8
aluno <aluno@liict-HP-EliteOne-800-G1-AiO>
2014-10-31 13:00:48 -0200

    Teste com velocidade de impressao de array inteiros

---
+++ Trabs_Sala/TesteVelocidadeImpressao/TesteInicializacao.java
@@ -0,0 +1,44 @@
+public class TesteInicializacao{
+  public static void main(String[] args) throws Exception{
+    int[] jjjj = {1,2,3,4,5};
+    imprimeArray(new int[]{1,32,231421345,3544352});
+  }
+  
+  private static void imprimeArray(int[] array) throws Exception{
+    System.out.println("Inicio algoritmo 1");    
+    long timestamp = System.nanoTime();
+    
+    String out = new String("[");
+    for(int c : array) out = out + c + ",";
+    out = out.substring(0,out.lastIndexOf(','));
+    out = out + "]";
+    System.out.println(out);
+    
+    timestamp = System.nanoTime() - timestamp;    
+    System.out.println("Termino 1, tempo: " + String.valueOf(timestamp) + " ns");
+    System.out.println("Inicio algoritmo 2");
+    timestamp = System.nanoTime();
+    
+    System.out.print("[");
+    for(int i = 0; i < array.length - 1; i++) System.out.print(array[i] + ",");
+    System.out.print(array[array.length - 1] + "]\n");
+    timestamp = System.nanoTime() - timestamp;
+    
+    System.out.println("Termino 2, tempo: " + String.valueOf(timestamp) + " ns"); 
+    
+    System.out.println("Inicio algoritmo 3");
+    timestamp = System.nanoTime();
+    
+    System.out.print("[");
+    for(int i = 0; i < array.length - 1; i++) {
+      System.out.print(array[i]);
+      System.out.print(",");
+    }
+    System.out.print(array[array.length - 1]);
+    System.out.print("]\n");
+    timestamp = System.nanoTime() - timestamp;
+    
+    System.out.println("Termino 3, tempo: " + String.valueOf(timestamp) + " ns"); 
+  }
+
+}
